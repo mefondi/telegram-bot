d720ea964d490ec4d292cebd0c09c47767f48025
mefondi <sad01072005@gmail.com>
2024-07-02 21:17:53 +0600

    the created catalog and product cards

--- app/handlers.py
+++ app/handlers.py
@@ -8,68 +8,29 @@ import app.keyboards as kb
 import app.database.requests as rq
 
 router = Router()
-
-class Register(StatesGroup):
-    name = State()
-    age = State()
-    number = State()
-
+ 
 @router.message(CommandStart())
 async def cmd_start(message: Message):
     await rq.set_user(message.from_user.id)
     await message.answer('привет, это магазин кроссоовок', reply_markup=kb.main)
 
-@router.message(Command('help'))
-async def cmd_start(message: Message):
-    await message.answer('кнопка помощи')
-
-@router.message(F.text == 'z')
-async def zov(message: Message):
-    await message.answer('zov')
-
-@router.message()
-async def repit(message: Message):
-    await message.answer(message.text)
-
-
-
 @router.message(F.text == 'каталог')
-async def zov(message: Message):
-    await message.answer('выберите категорию товара', reply_markup=kb.catalog)
-
-@router.callback_query(F.data == 't-shirt')
-async def t_shirt(callback: callback_query):
-    await callback.answer('Вы выбрали категорию футболок')
-    await callback.message.answer('Вы выбрали категорию футболок')
+async def catelog(message: Message):
+    await message.answer('выберите категорию товара', reply_markup=await kb.categories())
 
 
+@router.callback_query(F.data.startswith('category_'))
+async def catelog(callback: callback_query):
+    await callback.answer('вы выбрали категорию')
+    await callback.message.answer('выберите товар по категории',
+                                  reply_markup=await kb.items(callback.data.split('_')[1]))
 
-@router.message(Command('register'))
-async def register(message: Message, state:FSMContext):
-    await state.set_state(Register.name)
-    await message.answer('введите ваше имя')
-
-@router.message(Register.name)
-async def register_name(message: Message, state:FSMContext):
-    await state.update_data(name=message.text)
-    await state.set_state(Register.age)
-    await message.answer('введите ваш возраст')
-
-
-@router.message(Register.age)
-async def register_age(message: Message, state:FSMContext):
-    await state.update_data(age=message.text)
-    await state.set_state(Register.number)
-    await message.answer('введите ваш номер телефона', reply_markup=kb.get_number)
-
-
-@router.message(Register.number, F.contact)
-async def register_number(message: Message, state:FSMContext):
-    await state.update_data(number=message.contact.phone_number)
-    data = await state.get_data()
-    await message.answer(f"ваше имя {data['name']} ваш возраст {data['age']} ваш номер телефона {data['number']}")
-    await state.clear()
-
+@router.callback_query(F.data.startswith('item_'))
+async def catelog(callback: callback_query):
+    item_data = await rq.get_item(callback.data.split('_')[1])
+    await callback.answer('вы выбрали товар')
+    await callback.message.answer(f"Название: {item_data.name}\nОписание: {item_data.description}\nЦена: {item_data.price} руб.",
+                                   reply_markup=await kb.items(callback.data.split('_')[1]))
 
 
 
--- index.py
+++ index.py
@@ -9,7 +9,7 @@ from app.database.models import async_main
 
 async def main():
     await async_main()
-    bot = Bot(token='7168793031:AAE-sqCrG2Z-BOOwUCb6dA6vzwGVQbgDvtM')
+    bot = Bot(token='7168793031:AAGUb9tIbtmSgUTc2H1ZpLS2AR7n70fEeks')
     dp = Dispatcher()
     dp.include_router(router)
     await dp.start_polling(bot)
